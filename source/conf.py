5074a553f4607d1d8586ac7aa233a4f0b121ce19
henrysting <henrysting@hotmail.com>
2020-10-09 16:41:33 +0800

    adjust for readthedocs

--- source/conf.py
+++ source/conf.py
@@ -24,6 +24,7 @@ author = 'BNU-Astro'
 # The full version, including alpha/beta/rc tags
 release = '0.1'
 
+master_doc = 'index'
 
 # -- General configuration ---------------------------------------------------
 
